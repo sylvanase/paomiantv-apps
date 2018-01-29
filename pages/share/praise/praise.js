561d3e3c7098c6e28cf83729e4931efbf2c3d29a
sylvanase <362466464@qq.com>
2018-01-29 11:05:20 +0800

    全部静态页面完成

--- pages/share/praise/praise.js
+++ pages/share/praise/praise.js
@@ -4,15 +4,50 @@ const app = getApp()
 
 Page({
     data: {
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
         recordList:[
             {src:'',id:'',name:'尼克',time:'1-11 8:09', num:'8.0'},
             {src:'',id:'',name:'尼克',time:'1-11 8:09', num:'8.0'},
             {src:'',id:'',name:'尼克',time:'1-11 8:09', num:'8.0'},
             {src:'',id:'',name:'尼克',time:'1-11 8:09', num:'8.0'}
         ],
-        status: 3
+        status: 2
     },
     onLoad: function () {
-
+        if (app.globalData.userInfo) {
+            this.setData({
+                userInfo: app.globalData.userInfo,
+                hasUserInfo: true
+            })
+        } else if (this.data.canIUse) {
+            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+            // 所以此处加入 callback 以防止这种情况
+            app.userInfoReadyCallback = res => {
+                this.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                })
+            }
+        } else {
+            // 在没有 open-type=getUserInfo 版本的兼容处理
+            wx.getUserInfo({
+                success: res => {
+                    app.globalData.userInfo = res.userInfo
+                    this.setData({
+                        userInfo: res.userInfo,
+                        hasUserInfo: true
+                    })
+                }
+            })
+        }
+    },
+    getUserInfo: function (e) { // 获取用户信息
+        app.globalData.userInfo = e.detail.userInfo
+        this.setData({
+            userInfo: e.detail.userInfo,
+            hasUserInfo: true
+        })
     }
 })
